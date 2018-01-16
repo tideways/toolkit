85e83107f339a12507f0989a3b0dd6d4354c7517
Sina Mahmoodi <sina@tideways-gmbh.com>
2018-01-16 16:12:58 +0100

    Add callgrind parser

---
+++ xhprof/callgrind_test.go
@@ -0,0 +1,53 @@
+package xhprof
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseCallgrind(t *testing.T) {
+	expected := Profile{
+		Calls: []*Call{
+			&Call{
+				Name:              "main()",
+				Count:             1,
+				WallTime:          820,
+				ExclusiveWallTime: 20,
+			},
+			&Call{
+				Name:              "func2",
+				Count:             5,
+				WallTime:          700,
+				ExclusiveWallTime: 700,
+			},
+			&Call{
+				Name:              "func1",
+				Count:             1,
+				WallTime:          400,
+				ExclusiveWallTime: 100,
+			},
+		},
+	}
+
+	f, err := os.Open("testdata/callgrind-simple.out")
+
+	require.Nil(t, err)
+
+	profile, err := ParseCallgrind(f)
+
+	require.Nil(t, err)
+	require.NotNil(t, profile)
+	require.Len(t, profile.Calls, len(expected.Calls))
+
+	require.NotNil(t, profile.Main)
+	assert.EqualValues(t, profile.Main, expected.Calls[0])
+
+	profile.SortBy("WallTime")
+
+	for i, c := range profile.Calls {
+		assert.EqualValues(t, expected.Calls[i], c)
+	}
+}
